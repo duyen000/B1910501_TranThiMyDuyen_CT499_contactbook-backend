190cb90000ce50cc3d66c08f39da20ce9d59b756
tranthimyduyen <duyenb1910501@student.ctu.edu.vn>
2023-10-06 08:14:41 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/monngodb.util");
 
+async function startServer(){
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running om port ${PORT}`);
+        });
+    }catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
 //start server
-const PORT = config.app.port;
-app.listen(PORT,() => {
-    console.log('Server is running on port ${PORT}.');
-});
+//const PORT = config.app.port;
+//app.listen(PORT,() => {
+ //   console.log('Server is running on port ${PORT}.');
+//});
